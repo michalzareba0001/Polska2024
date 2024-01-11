6cba4614930a7e73a4ffa71ae026b34ee4a478f6
Narrator-ka <156190097+Narrator-ka@users.noreply.github.com>
2024-01-11 21:36:29 +0100

    Update questions.js
    7-8

--- data/questions.js
+++ data/questions.js
@@ -76,28 +76,26 @@ const questions = [
         image: require('../assets/images/question7.jpg'),
       },
           {
-        questionText: 'Uchodźcy z Ukrainy czują się niechciani. Sytuacja między Polakami i Ukraińcami robi się napięta. Jak ją załagodzisz?',
-        answers: [
-          { text: 'Nakażesz stacjom radiowym robić wiadomości po ukraińsku.', points: { finanse: -3, popularnosc: -2, obrona: 1, dyplomacja: 2 } },
-          { text: 'Nie zareagujesz. Będziesz obserwować.', points: { finanse: 0, popularnosc: -3, obrona: 0, dyplomacja: -1 } },
-          { text: 'Zafundujesz rozwój kompetencji, żeby zatrzeć różnice społeczne.', points: { finanse: 0, popularnosc: -5, obrona: 2, dyplomacja: 0 } },
-          { text: 'Opłacisz trolle internetowe, każąc im przekierować nienawiść na inną mniejszość.', points: { finanse: 5, popularnosc: 1, obrona: 0, dyplomacja: +3 } },
-          
-        ],
-        image: require('../assets/images/question3.jpg'),
-      },
-          {
         questionText: 'Mikro-przedsiębiorcy zaczynają strajkować przeciw opłatom na ZUS, wstrzymując płatność podatków. Jak reagujesz?',
         answers: [
-          { text: 'Zarządzasz ustawą miesiąc wakacji od ZUS.', points: { finanse: -3, popularnosc: -2, obrona: 1, dyplomacja: 2 } },
-          { text: 'Nie zareagujesz. Będziesz obserwować.', points: { finanse: 0, popularnosc: -3, obrona: 0, dyplomacja: -1 } },
-          { text: 'Zafundujesz rozwój kompetencji, żeby zatrzeć różnice społeczne.', points: { finanse: 0, popularnosc: -5, obrona: 2, dyplomacja: 0 } },
-          { text: 'Opłacisz trolle internetowe, każąc im przekierować nienawiść na inną mniejszość.', points: { finanse: 5, popularnosc: 1, obrona: 0, dyplomacja: +3 } },
+          { text: 'Zarządzasz ustawą miesiąc wakacji od ZUS.', points: { finanse: -2, popularnosc: 2, obrona: 0, dyplomacja: 0 } },
+          { text: 'Na poważnie planujesz reformę i obiecujesz opcjonalny ZUS.', points: { finanse: -3, popularnosc: 5, obrona: 0, dyplomacja: 1 } },
+          { text: 'Dajesz dużą ulgę od ZUS jednej grupie zawodowej.', points: { finanse: -1, popularnosc: -4, obrona: -1, dyplomacja: 2 } },
+          { text: 'Czekasz aż ochłoną.', points: { finanse: -1, popularnosc: -5, obrona: -3, dyplomacja: -1 } },
+          
+        ],
+        image: require('../assets/images/question8.jpg'),
+                {
+          questionText: 'Brakuje rąk do pracy, bo Zetki wciąż zastanawiają się jaka jest ich „prawdziwa droga życia". Co teraz?',
+        answers: [
+          { text: 'Jak to co? Trzeba sprzedać wizy azjatom!', points: { finanse: 3, popularnosc: -2, obrona: -3, dyplomacja: 2 } },
+          { text: 'Ściągasz ekspatów ciekawymi ulgami i dotacjami.', points: { finanse: -1, popularnosc: 3, obrona: 0, dyplomacja: 2 } },
+          { text: 'Zwiększasz podatki, które doprowadzą do masowych zwolnień i bezrobotni się znajdą.', points: { finanse: 1, popularnosc: -5, obrona: -2, dyplomacja: -3 } },
+          { text: 'Nic. Niech przedsiębiorcy sobie jakoś radzą.', points: { finanse: 0, popularnosc: -1, obrona: 0, dyplomacja: -1 } },
           
         ],
         image: require('../assets/images/question3.jpg'),
       },
-  ];
   
   export default questions;
   
