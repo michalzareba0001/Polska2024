b355b5566fbc76d2c0aafb07c69403b7b556715b
Michal <michalzareba0001@gmail.com>
2024-01-17 14:58:15 +0100

    zakres punktów i poprawki do końca gry

--- components/EndGameObrona.jsx
+++ components/EndGameObrona.jsx
@@ -4,7 +4,8 @@ import React from 'react'
 const EndGameObrona = () => {
   return (
       <View style={styles.EndGameContainer}>
-      <Text>GAME OVER</Text>
+      <Text style={styles.EndGameTitle}>GRA ZAKOŃCZONA</Text>
+      <Text style={styles.EndGameOpis}>Ponieważ twoja obronność spadła do zera! itp itd lorem ipsum lorem ipsum lorem ipsum lorem ipsum</Text>
       </View>
   )
 }
@@ -19,5 +20,16 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  
+  EndGameTitle: {
+    fontSize: 20,
+    fontWeight: '900'
+  },
+
+  EndGameOpis: {
+    fontSize: 15,
+    paddingLeft: 20,
+    paddingRight: 20,
+  }
   
 })
--- screens/GameBoard.jsx
+++ screens/GameBoard.jsx
@@ -10,26 +10,62 @@ import EndGameFinanse from '../components/EndGameFinanse';
 import EndGameObrona from '../components/EndGameObrona';
 import EndGamePopularnosc from '../components/EndGamePopularnosc';
 import EndGameDyplomacja from '../components/EndGameDyplomacja';
+import LowLifeModal from '../components/LowLifeModal'
 
 const GameBoard = () => {
-    const [finanse, setFinanse] = useState(50);
+    const [finanse, setFinanse] = useState(99);
     const [popularnosc, setPopularnosc] = useState(50);
     const [obrona, setObrona] = useState(50);
     const [dyplomacja, setDyplomacja] = useState(50);
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [shuffledQuestions, setShuffledQuestions] = useState([]);
+    const [isModalVisible, setModalVisible] = useState(false);
     const [pointsForCurrentQuestion, setPointsForCurrentQuestion] = useState({
         finanse: 0,
         popularnosc: 0,
         obrona: 0,
         dyplomacja: 0,
     });
-    const [endGameCalled, setEndGameCalled] = useState(false);
+    
+    //zakres punktów
+    if (finanse > 100) {
+        setFinanse(100)
+    }
+
+    if (popularnosc > 100){
+        setPopularnosc(100)
+    }
+    
+    if (obrona > 100){
+        setObrona(100)
+    }
+
+    if (dyplomacja > 100){
+        setDyplomacja(100)
+    }
+
+    if (finanse < 0) {
+        setFinanse(0)
+    }
+
+    if (popularnosc < 0){
+        setPopularnosc(0)
+    }
+    
+    if (obrona < 0){
+        setObrona(0)
+    }
+
+    if (dyplomacja < 0){
+        setDyplomacja(0)
+    }
+
 
     useEffect(() => {
         // Potasuj pytania przed rozpoczęciem gry
         const shuffled = [...questions].sort(() => Math.random() - 0.5);
         setShuffledQuestions(shuffled);
+       
     }, []);
 
 
@@ -39,7 +75,8 @@ const GameBoard = () => {
         setPopularnosc(popularnosc + points.popularnosc);
         setObrona(obrona + points.obrona);
         setDyplomacja(dyplomacja + points.dyplomacja);
-
+        
+       
         // Zapisz punkty zdobyte w aktualnym pytaniu
         setPointsForCurrentQuestion({
             finanse: points.finanse,
@@ -48,6 +85,7 @@ const GameBoard = () => {
             dyplomacja: points.dyplomacja,
         });
 
+            
         // Przejdź do następnego pytania
         setCurrentQuestion(currentQuestion + 1);
 
@@ -59,13 +97,24 @@ const GameBoard = () => {
                 dyplomacja: 0,
             });
         }, 2000);
+
     };
+
+    const openModal = () => {
+        setModalVisible(true);
+      };
+      
+      const closeModal = () => {
+        setModalVisible(false);
+      };
 
     return (
         <ImageBackground source={gameboardBg} style={styles.backgroundImg}>
             <View style={styles.container}>
                 {/* Górna część - Pytania */}
             <View style={styles.upperSection}>
+            <LowLifeModal isVisible={isModalVisible} onClose={closeModal} content="To jest treść modala!" />
+
                 {currentQuestion < shuffledQuestions.length && finanse >= 1 && popularnosc >=1 && obrona >= 1 && dyplomacja >= 1 ? (
                     <>
                         <Text style={styles.dataText}>Dzień: {currentQuestion + 1}</Text>
@@ -83,18 +132,15 @@ const GameBoard = () => {
                     <EndGameFinanse/>
                 ) : currentQuestion < shuffledQuestions.length && popularnosc < 1 ? (
                     <EndGamePopularnosc/>
-                ) : currentQuestion < shuffledQuestions.length && obrona < 1 ? (
-                    <EndGameObrona/>
-                ) : currentQuestion < shuffledQuestions.length && dyplomacja < 1 ? (
+                    ) : currentQuestion < shuffledQuestions.length && obrona < 1 ? (
+                        <EndGameObrona/>
+                        ) : currentQuestion < shuffledQuestions.length && dyplomacja < 1 ? (
                     <EndGameDyplomacja/>
                 ) : (
                     <Text style={styles.endGameText}>
                         Gra zakończona. Twój wynik, finanse: {finanse}, popularność:{popularnosc}, obrona:{obrona}, dyplomacja: {dyplomacja}
                     </Text>
                 )}
-            
-
-            
 
                 </View>
 
@@ -306,124 +352,3 @@ const styles = StyleSheet.create({
 });
 
 export default GameBoard;
-
-
-// const styles = StyleSheet.create({
-//     dataText: {
-//         color: '#fff',
-//         marginTop: 10,
-//         fontSize: 16,
-//         fontWeight: '900',
-//         textTransform: 'uppercase',
-//     },
-
-//     container: {
-//         flex: 1,
-//         justifyContent: 'space-between',
-//         paddingTop: 20,
-//     },
-
-//     upperSection: {
-//         flex: 1,
-//         justifyContent: 'flex-start',
-//         alignItems: 'center',
-//     },
-
-//     lowerSection: {
-//         borderTopColor: '#ccc',
-//         alignItems: 'center',
-//         display: 'flex',
-//         justifyContent: 'flex-end',
-//         alignItems: 'flex-end',
-//         flexDirection: 'row',
-//         height: 100,
-//     },
-
-//     pictureContainer: {
-//         height: '100%',
-//         width: '25%',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//     },
-
-//     lowerSectionText: {
-//         fontSize: 16,
-//         fontWeight: '700',
-//         color: '#ffffff'
-//     },
-
-//     image: {
-//         width: '100%',
-//         height: 250,
-//         resizeMode: 'cover',
-//         marginBottom: 20,
-//         marginTop: 10,
-//     },
-
-//     textContainer: {
-//         width: '100%',
-//         paddingLeft: 20,
-//         paddingRight: 20,
-//     },
-
-//     answerBtn: {
-//         width: '95%',
-//         display: 'flex',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//         marginTop: 10,
-//         padding: 9,
-//         backgroundColor: '#ffffff', // Dodaj kolor tła, aby przypominał przycisk
-//         borderRadius: 10, // Zaokrąglij rogi
-//         borderColor: '#F60000',
-//         borderWidth: 1,
-//     },
-
-//     answerBtnText: {
-//         fontSize: 13,
-//         color: '#000000',
-//         fontWeight: '700',
-//         textAlign: 'center',
-//     },
-
-//     questionText: {
-//         fontSize: 17,
-//         fontWeight: '600',
-//         marginBottom: 20,
-//         textShadowColor: '#FFFFFF80',
-//         textShadowRadius: 10,
-
-
-//     },
-
-//     endGameText: {
-//         fontSize: 16,
-//         fontWeight: '700',
-//         marginTop: '50%',
-//     },
-
-//     backgroundImg: {
-//         height: '100%',
-//         width: '100%',
-//         resizeMode: 'cover',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//     },
-//     scoreIcon: {
-//         width: 40,
-//         height: 40,
-//         zIndex: 500,
-//         alignSelf: 'center',
-//         opacity: 1,
-
-//     },
-//     scoreDiff: {
-//         position: 'absolute',
-//         bottom: 10,
-//         left: 10,
-//         fontWeight: '900',
-//     },
-// });
-
-// export default GameBoard;
