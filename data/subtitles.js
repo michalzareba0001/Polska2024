fbe4ad49300756bdddc082e69f6429f1de3b267b
Michal <michalzareba0001@gmail.com>
2024-01-17 16:54:33 +0100

    Text do gry

---
+++ data/subtitles.js
@@ -0,0 +1,38 @@
+const subtitles = [
+    {
+        finanseLow: [
+            { descText: 'Grozi Ci ruina! Tnij socjal, bo Polska przepadnie!' },
+            { descText: 'Program Willa+ znacząco zwiększa deficyt budżetowy! Uważaj!' }
+        ],
+        finanseVeryLow: [
+            { descText: 'UE odmawia Ci dalszych pożyczek!' },
+            { descText: 'Dryfujesz po bezkresnym morzu bankructwa! Jak Grecja!' }
+        ],
+        popularnoscLow: [
+            { descText: 'Jesteś popularny niczym Macierewicz albo Braun…' },
+            { descText: 'Młodzież pokazuje Ci koniec kolejki, jak Kaczyńskiemu.' }
+        ],
+        popularnoscVeryLow: [
+            { descText: 'Zaraz wywiozą Cię z sejmu na taczce!' },
+            { descText: 'Nie wychodź z domu bez kamuflażu.' }
+        ],
+        obronaLow: [
+            { descText: 'Czy aby dobre słowo jest najlepszym orężem? Jakiś czołg też by się przydał…' },
+            { descText: 'Rakiety odnajdują się w naszych lasach po roku albo dwóch… Zadbaj o obronę!' }
+        ],
+        obronaVeryLow: [
+            { descText: 'Rosyjscy szpiedzy grzeją tyłki na sejmowych ławach.' },
+            { descText: 'Planujesz wprowadzić stan wojenny?' }
+        ],
+        dyplomacjaLow: [
+            { descText: 'Nazywasz swoich azjatyckich gości „Szogunami”?' },
+            { descText: 'Głośno opowiadasz o tym, że LGBT to nie ludzie, tylko ideologia — czego się spodziewasz?' }
+        ],
+        dyplomacjaVeryLow: [
+            { descText: 'Przysypiasz podczas obrad sejmu i przemówień swoich zagranicznych gości.' },
+            { descText: 'Głośno zastanawiasz się nad możliwym przejęciem fragmentu sąsiedniego kraju.' }
+        ],
+    }
+]
+
+export default subtitles
